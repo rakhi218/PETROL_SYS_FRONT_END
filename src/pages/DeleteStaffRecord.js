157e88c99edabf87920fe1dccb7134ca86bcb098
Rakesh Vanga <rakesh_vanga@persistent.com>
2023-01-28 17:04:18 +0530

    integrated master delete and product management

--- src/pages/DeleteStaffRecord.js
+++ src/pages/DeleteStaffRecord.js
@@ -1,41 +1,51 @@
 import axios from 'axios';
-import React, { useEffect } from 'react'
+import React, { useState,useEffect } from 'react'
 //import './DeleteStaffRecord.css'
 
-const handleSubmit = async(event) => {
-  // event.preventDefault();
-  // const data = new FormData(event.currentTarget);
-  // try {
-  //   const res = await axios.delete("https://localhost:7086/api/StaffAttendance",{tblStaffId: data.get('')});
-  // }catch(err){
-  //   console.log(err);
-  // }
+const showDetails = async(event) => {
+  const obj  = {
+    tblStaffId : document.getElementById("StaffId").value,
+    tblDate : document.getElementById("AttaindanceDate").value
+  }
+  try {
+    const res = await axios.get("https://localhost:7086/api/StaffAttendance",obj);
+    console.log(res);
+  }catch(err){
+    console.log(err);
+  }
 }
 
 function DeleteStaffRecord (){
 
-  // useEffect(() => {
-  //   async function GetStaffData() {
-  //     try {
-  //       const res = await axios.get("https://localhost:7086/api/StaffAttendance",{tblStaffId: "CS-2001-005",tblDate : Date().toLocaleString()});
-  //       console.log(res);
-  //     }catch(err){
-  //       console.log(err);
-  //     }
-  //   }
-  //   GetStaffData();
-  // })
+  const [staffs,setStaffs] = useState([]);
+
+  useEffect(() => {
+    async function GetStaffData() {
+      try {
+        const res = await axios.get("https://localhost:7215/api/staff");
+        console.log(res);
+        setStaffs(res.data);
+      }catch(err){
+        console.log(err);
+      }
+    }
+    GetStaffData();
+  },[])
 
   return (
     <>
     <div>
     <h1 className= "head" align = "center">Delete Staff Record </h1>
-    <form className="formclassName" onSubmit={handleSubmit}>
+    <form className="formclassName">
             <div>
         <label>Staff ID </label>
-        <select className ="button">   <option selected>Choose...</option>
-                    <option>...</option>
-                 </select>
+        <select className ="button" name="StaffIDs" id="StaffId">
+          {staffs.map((record) => (
+            <option key={record.tblStaffID} value={record.tblStaffID} id="tblStaffId">
+              {record.tblStaffID}
+            </option>
+          ))}
+        </select>
                         
     </div>
     <div>
@@ -44,13 +54,14 @@ function DeleteStaffRecord (){
     </div>
     <div>
         <label>Date </label>
-    <input type="datetime-local" />
+    <input type="date" id="AttaindanceDate"/>
     </div>
     <div>
     <label>Pump </label>
     <input type="text" id="pump" name="Pump" className = "textbox" />&ensp;&ensp;
     </div>
     <button type = "submit" className="button">Delete </button>
+    <button type = "button" className="button" onClick={showDetails}> Show Details </button>
     </form>
     </div>
     </>
--- src/pages/MasterDelete.js
+++ src/pages/MasterDelete.js
@@ -7,22 +7,22 @@ import { ClickAwayListener } from '@mui/material';
 
 function MasterDelete() {
 
-    const [data ,setUnlockClicked] = useState("");
-    const [pumpIds,setPumpIds] = useState("");
+    const [readings ,setReadings] = useState([]);
+    const [pumps,setPumps] = useState([]);
 
     useEffect(() => {
         
         async function getAllPumpsRecord() {
             try {
                 const res = await axios.get("https://localhost:7144/api/PumpManagement");
-                setUnlockClicked(res.data);
-                const p=[];
-                console.log(res.data)
-                res.data.map((record) => {
-                    p.push(record.tblPumpID);
-                });
-                setPumpIds(p);
-
+                const l=[]
+                const r=[]
+                await res.data.map((record) => {
+                    l.push(record.tblPumpID);
+                    r.push(record.tblLastReading);
+                })
+                setPumps(l);
+                setReadings(r);
             }catch(err){
                 console.log(err)
             }
@@ -30,7 +30,42 @@ function MasterDelete() {
         getAllPumpsRecord();
     },[])
 
+     
+    async function Populate() {
+        const index = document.getElementById("PumpIDS").value;
+        document.getElementById("lastreading").value = readings[index-1];
+        console.log(index);
+        console.log(readings)
+        const obj ={
+            tblPumpID:index,
+            tblFinalLitres:readings[index-1]
+        }
+        const res = await axios.post("https://localhost:7086/api/RemovePump",obj);
+        console.log(res.data);
+        document.getElementById("PumpNumber").value = res.data.tblPumpID;
+        document.getElementById("PumpDate1").value = res.data.tblDate1;
+        document.getElementById("PumpInitialReading").value = res.data.tblInitialLitres;
+        document.getElementById("PumpLastReading").value = res.data.tblFinalLitres;
+        document.getElementById("PumpSaleNumber").value = res.data.tblPumpID;
+        document.getElementById("PumpDate2").value =  res.data.tblDate2;
+        document.getElementById("StaffID").value = res.data.tblStaffID;
+        document.getElementById("PumpShift").value = res.data.tblShift;
+    }
     
+    async function deleteForm() {
+        const obj = {
+            tblPumpID: document.getElementById("PumpNumber").value,
+            tblStaffID : document.getElementById("StaffID").value,
+            tblDate2 : document.getElementById("PumpDate2").value,
+            tblFinalLitres : document.getElementById("PumpLastReading").value,
+            tblInitialLitres : document.getElementById("PumpInitialReading").value,
+            tblDate1 : document.getElementById("PumpDate1").value,
+            tblShift : document.getElementById("PumpShift").value
+        }
+        const res = await axios.post("https://localhost:7086/api/RemovePump/deletepump",obj);
+        console.log(res);
+    }
+
     return (
         
         <>
@@ -40,42 +75,43 @@ function MasterDelete() {
             <div>
         <label>Select Pump </label>
         <select className ="button" name="PumpID" id="PumpIDS"> 
-        {/* {data.map((record) => (
-            <option key={record.tblPumpID} value={record.tblPumpID}>
+        {pumps.map((record) => (
+            <option key={record} value={record} id="pumps">
+                {record}
             </option>
-        ))} */}
+        ))}
         </select>
         
     </div>
     <label>Last Reading </label>
      <input type="text" id="lastreading" name="Last Reading" className = "textbox"/>&ensp;&ensp;
-     <button type = "submit" className="button">Show</button>
+     <button type = "button" className="button" onClick={Populate}>Show</button>
 
      <div>
         <text>Pump Record Detail </text> <br />
         <label>Pump Number </label>
-     <input type="text" id="lastreading" name="Pump Number" className = "textbox" /><br/>
+     <input type="text" id="PumpNumber" name="Pump Number" className = "textbox" /><br/>
      <label>Date </label>
-     <input type="text" id="lastreading" name="Date" className = "textbox"/><br/>
+     <input type="datetime" id="PumpDate1" name="Date" className = "textbox"/><br/>
      <label>Initial Reading </label>
-     <input type="text" id="lastreading" name="Initial Reading" className = "textbox" /><br/>
+     <input type="text" id="PumpInitialReading" name="Initial Reading" className = "textbox" /><br/>
      <label>Final Reading </label>
-     <input type="text" id="lastreading" name="Final Reading" /><br/>
+     <input type="text" id="PumpLastReading" name="Final Reading" /><br/>
      </div>
 
 
      <div> 
         <text>Sales Record Detail </text> <br />
         <label>Pump Number </label>
-     <input type="text" id="lastreading" name="Pump Number" /><br/>
+     <input type="text" id="PumpSaleNumber" name="Pump Number" /><br/>
      <label>Date </label>
-     <input type="text" id="lastreading" name="Date" /><br/>
-     <label>Staff Name </label>
-     <input type="text" id="lastreading" name="Staff Name" /><br/>
+     <input type="datetime" id="PumpDate2" name="Date" /><br/>
+     <label>Staff ID </label>
+     <input type="text" id="StaffID" name="Staff Name" /><br/>
      <label>Shift </label>
-     <input type="text" id="lastreading" name="Shift" /><br/>
+     <input type="text" id="PumpShift" name="Shift" /><br/>
      </div>
-     <button type = "submit" className="button">Delete</button>
+     <button type = "button" className="button" onClick={deleteForm}>Delete</button>
 
             </form>
             
